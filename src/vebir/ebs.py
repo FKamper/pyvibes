2872cd40da167a3c4f9107573ec93c886f3f3e0e
FKamper <francois.kamper@gmail.com>
2025-07-30 11:31:28 +0200

    added docstrings to ebs.py

--- src/vebir/ebs.py
+++ src/vebir/ebs.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 def ebs_als(y, V, tau=0.1, mit=100, verbose=False):
     """
     Reconstructs the interference present in a spectra.
-    Given a spectra `y` and a (scaled) right singular vector matrix `V`, this function finds the coefficient vector `x`
+    Given a spectrum `y` and a (scaled) right singular vector matrix `V`, this function finds the coefficient vector `x`
     that minimizes the asymmetrically weighted least squares (ALS) loss function charaterized by `tau`. The optimization
     is performed iteratively until convergence or until the maximum number of iterations is reached.
     Args:
@@ -39,8 +39,8 @@ def ebs_als(y, V, tau=0.1, mit=100, verbose=False):
 
 def ebs_pb(y, V, tau=0.1, verbose=False):
     """
-    Reconstructs the interference present in a spectra.
-    Given a spectra `y` and a (scaled) right singular vector matrix `V`, this function finds the coefficient vector `x`
+    Reconstructs the interference present in a spectrum.
+    Given a spectrum `y` and a (scaled) right singular vector matrix `V`, this function finds the coefficient vector `x`
     that minimizes the pinball loss function charaterized by `tau`. The optimization
     is performed using convex optimization with the CLARABEL solver.
     Args:
@@ -63,7 +63,54 @@ def ebs_pb(y, V, tau=0.1, verbose=False):
 
 
 class EBS:
-    """Eliminate Background Spectrum."""
+    """
+    Eliminate Background Spectrum (EBS) algorithm for interference remocal in spectral data.
+    Parameters
+    ----------
+    tau : float, optional
+        Parameter controlling the asymmetry of the loss function (default is 0.1).
+    loss : str, optional
+        Loss function to use for background estimation. Options are:
+            - "PB": Pinball Loss
+            - "ALS": Asymmetric Weighted Least Squares
+        Default is "PB".
+    mit : int, optional
+        Maximum number of iterations for the ALS algorithm (default is 100).
+    Attributes
+    ----------
+    tau : float
+        Asymmetric parameter.
+    mit : int
+        Maximum number of iterations.
+    loss : str
+        Selected loss function.
+    x : ndarray
+        Estimated weights or coefficients from the background estimation.
+    interference : ndarray
+        Estimated interference spectrum.
+    absorbance : ndarray
+        Estimated absorbance spectrum.
+    Methods
+    -------
+    fit(y, mu, W)
+        Estimate and remove intereference from the observed spectrum.
+        Parameters
+        ----------
+        y : ndarray
+            Observed spectrum.
+        mu : ndarray
+            Mean interference spectrum. Set to zero if centering is not applied.
+        W : ndarray
+            Scaled right singular vector matrix W = VD obtained from a SVD X - mu = UDV' of the interference examples. Take W = V if no scaling is applied.
+        Updates
+        -------
+        self.x : ndarray
+            Estimated latent loading coefficients.
+        self.interference : ndarray
+            Estimated interference spectrum.
+        self.absorbance : ndarray
+            Estimated absorbance spectrum.
+    """
 
     def __init__(self, tau=0.1, loss="PB", mit=100):
         self.tau = tau
