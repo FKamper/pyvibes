af7186af8911796c343f9cdb2f52930ceb15e89a
FKamper <francois.kamper@gmail.com>
2025-10-08 15:15:05 +0200

    minor updates

--- scripts/run_veb_pb.py
+++ scripts/run_veb_pb.py
@@ -1,92 +1,13 @@
 import pickle
 import numpy as np
-import time
 import multiprocessing as mp
 import pandas as pd
 
 from pathlib import Path
 from tqdm import tqdm
-from vebir.absorbance_estimators.veb import VEB
 from vebir.utils.metrics import compute_method_cors
 from vebir.interference_models.pca import pca, loo_pca
-
-
-def distribute_function(args):
-    y, mu, W, tau_min, tau_max, loss = args
-
-    mod = VEB(c=W.shape[1], loss=loss)
-    start = time.time()
-    mod.fit(y, mu, W, tau_min=tau_min, tau_max=tau_max)
-    mod.map(y, mu, W)
-    end = time.time()
-
-    res = {
-        "absorbance": mod.absorbance,
-        "time": end - start,
-        "tau": mod.tau,
-        "c": mod.c,
-        "sigma_hat": mod.sigma_hat,
-        "nu": mod.nu,
-        "d": mod.d,
-    }
-
-    return res
-
-
-def distribute_veb(args):
-    y, mu, W, tau_min, tau_max, loss, c_grid, mit = args
-
-    start = time.time()
-    mod = VEB(c=c_grid[0], loss=loss)
-    elbos = []
-    for i in range(len(c_grid)):
-        c = c_grid[i]
-        mod.c = c
-        mod.fit(y, mu, W[:, :c], mit=mit, tau_min=tau_min, tau_max=tau_max)
-        elbos.append(mod.elbo)
-
-        if elbos[-1] == np.max(elbos):
-            tau_init = mod.tau
-            chat = c
-            nu_init = mod.nu
-            d_init = mod.d
-
-        if i == len(c_grid) - 1:
-            break
-
-        mod.tau_init = mod.tau
-        mod.nu_init = np.append(mod.nu_init, np.zeros(c_grid[i + 1] - c))
-        mod.d_init = np.append(mod.d_init, np.ones(c_grid[i + 1] - c))
-
-    mod = VEB(
-        c=chat,
-        tau_init=tau_init,
-        nu_init=nu_init,
-        d_init=d_init,
-        loss=loss,
-    )
-    mod.fit(
-        y,
-        mu,
-        W[:, :chat],
-        tau_min=tau_min,
-        tau_max=tau_max,
-    )
-    mod.map(y, mu, W[:, :chat])
-    end = time.time()
-
-    res = {
-        "absorbance": mod.absorbance,
-        "time": end - start,
-        "tau": mod.tau,
-        "c": mod.c,
-        "sigma_hat": mod.sigma_hat,
-        "nu": mod.nu,
-        "d": mod.d,
-    }
-
-    return res
-
+from vebir.utils.distribute import distribute_veb, distribute_veb_c
 
 if __name__ == "__main__":
     REPO_ROOT = Path(__file__).resolve().parents[1]
@@ -137,7 +58,7 @@ if __name__ == "__main__":
             with mp.Pool(processes=mp.cpu_count()) as pool:
                 results = list(
                     tqdm(
-                        pool.imap(distribute_function, distribute_args),
+                        pool.imap(distribute_veb, distribute_args),
                         total=len(distribute_args),
                         desc=f"{comp}",
                     )
@@ -175,7 +96,7 @@ if __name__ == "__main__":
             with mp.Pool(processes=mp.cpu_count()) as pool:
                 results = list(
                     tqdm(
-                        pool.imap(distribute_function, distribute_args),
+                        pool.imap(distribute_veb, distribute_args),
                         total=len(distribute_args),
                         desc=f"{comp}",
                     )
@@ -215,7 +136,7 @@ if __name__ == "__main__":
             with mp.Pool(processes=mp.cpu_count()) as pool:
                 results = list(
                     tqdm(
-                        pool.imap(distribute_veb, distribute_args),
+                        pool.imap(distribute_veb_c, distribute_args),
                         total=len(distribute_args),
                         desc=f"{comp}",
                     )
